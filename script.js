ac25ef8adf5bc0d8baa9c3856c9d30192adef430
sandip786 <sandeepyadavj7865@gmail.com>
2023-12-11 20:52:23 +0530

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById("bar");
+const closes = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add("active")
+    })
+}
+if (closes) {
+    closes.addEventListener('click', () => {
+        nav.classList.remove("active")
+    })
+}
+
